936e8f814889f085fb50f0e2255d6b52c14850f7
teruterubozuuu <joylynnegrace@gmail.com>
2025-05-21 12:06:12 +0800

    fix: duplicate outputs when checking for prime numbers

--- demo.php
+++ demo.php
@@ -10,50 +10,50 @@
 
 //prime numbers
 
-// function isPrimeNumber($number){
-//     if($number< 2){
-//         echo "{$number} is not a prime number.\n";
+function isPrimeNumber($number){
+    if($number< 2){
+        return "{$number} is not a prime number.\n";
 
-//     }
-//     if ($number ==2){
-//         echo "{$number} is a prime number.\n" ;
+    }
+    if ($number ==2){
+        return "{$number} is a prime number.\n" ;
 
-//     }
-//     if ($number % 2 ==0){
-//         echo "{$number} is not a prime number.\n";
-//     }
+    }
+    if ($number % 2 ==0){
+        return "{$number} is not a prime number.\n";
+    }
 
-//     $sqrtNumber = sqrt($number);
-//     for($i=3; $i <= $sqrtNumber; $i+=2){
-//         if ($number % $i == 0){
-//             echo "{$number} is not a prime number.\n";
-//         }
-//     }
+    $sqrtNumber = sqrt($number);
+    for($i=3; $i <= $sqrtNumber; $i+=2){
+        if ($number % $i == 0){
+            return "{$number} is not a prime number.\n";
+        }
+    }
 
-//     return "{$number} is a prime number.\n";
-// }
+    return "{$number} is a prime number.\n";
+}
 
-// $number = readline("Enter a number: ");
-// if (!is_numeric($number)){
-//     echo "Invalid input";
-//     exit;
-// }
+$number = readline("Enter a number: ");
+if (!is_numeric($number)){
+    echo "Invalid input";
+    exit;
+}
 
-// $number = (int)$number;
-// for($ctr=0; $ctr<=$number; $ctr++){
-//     echo isPrimeNumber($ctr);
-// }
+$number = (int)$number;
+for($ctr=0; $ctr<=$number; $ctr++){
+    echo isPrimeNumber($ctr);
+}
 
-require __DIR__ . '/vendor/autoload.php';
-use Cocur\Slugify\Slugify;
-use Ramsey\Uuid\Uuid;
+// require __DIR__ . '/vendor/autoload.php';
+// use Cocur\Slugify\Slugify;
+// use Ramsey\Uuid\Uuid;
 
-$slugify = new Slugify();
-echo $slugify->slugify("This is the new title." . "\n");
+// $slugify = new Slugify();
+// echo $slugify->slugify("This is the new title." . "\n");
 
-$uuid = Uuid::uuid4();
-echo $uuid -> toString() . "\n";
+// $uuid = Uuid::uuid4();
+// echo $uuid -> toString() . "\n";
 
-$faker = Faker\Factory::create();
-echo $faker -> name() . "\n";
-echo $faker -> email() . "\n";
+// $faker = Faker\Factory::create();
+// echo $faker -> name() . "\n";
+// echo $faker -> email() . "\n";
